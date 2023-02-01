a951eab11528f4f6e767817e7e1f3dd6a9c85733
mabyre <mabyre@sodevlog.com>
2023-02-01 16:53:57 +0100

    Add ValidationRules to Page1

---
+++ WpfAppRibbonTemplateStudio/Validation/EmailValidationRule.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace WpfAppRibbonTemplateStudio.Validation
+{
+    public class EmailValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            // Is a valid email address?
+            var pattern =
+                @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*([,;]\s*\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)*";
+            if (!Regex.IsMatch((string)value, pattern))
+            {
+                var msg = $"{value} is not a valid email address.";
+                return new ValidationResult(false, msg);
+            }
+
+            // Email address is valid
+            return new ValidationResult(true, null);
+        }
+    }
+}
